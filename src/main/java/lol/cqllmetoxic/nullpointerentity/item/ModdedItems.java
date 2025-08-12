a017c05408e2f07b64ddd0e1c6a87e2f11464192
CqllMeToxic <idonuntknow@gmail.com>
2025-08-12 18:52:19 -0400

    Format code for AURORA_CORE item registration

--- src/main/java/lol/cqllmetoxic/nullpointerentity/item/ModdedItems.java
+++ src/main/java/lol/cqllmetoxic/nullpointerentity/item/ModdedItems.java
@@ -12,7 +12,7 @@ import net.minecraft.util.Identifier;
 
 
 public class ModdedItems {
-    public static final Item AURORA_CORE=registerItem("aurora_core",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID,"aurora_core")))));
+    public static final Item AURORA_CORE = registerItem("aurora_core",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID,"aurora_core")))));
     public static final Item CORRUPTED_DATA_SHARD = registerItem("corrupted_data_shard", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID, "corrupted_data_shard")))));
     public static final Item MEMORY_FRAGMENT = registerItem("memory_fragment", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID, "memory_fragment")))));
     public static final Item SYSTEM_OVERRIDE_CHIP = registerItem("system_override_chip", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID, "system_override_chip")))));
