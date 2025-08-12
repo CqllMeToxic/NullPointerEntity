c41098ebb9ec04fe577a84e8f88d74dda3e54b7c
CqllMeToxic <idonuntknow@gmail.com>
2025-08-12 05:57:05 -0400

    Add ModdedItems class to define custom items as well as add IDs for each item

---
+++ src/main/java/lol/cqllmetoxic/nullpointerentity/item/ModdedItems.java
@@ -0,0 +1,36 @@
+package lol.cqllmetoxic.nullpointerentity.item;
+
+import lol.cqllmetoxic.nullpointerentity.NullPointerEntity;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+
+public class ModdedItems {
+    public static final Item AURORA_CORE=registerItem("aurora_core",new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID,"aurora_core")))));
+    public static final Item CORRUPTED_DATA_SHARD = registerItem("corrupted_data_shard", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID, "corrupted_data_shard")))));
+    public static final Item MEMORY_FRAGMENT = registerItem("memory_fragment", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID, "memory_fragment")))));
+    public static final Item SYSTEM_OVERRIDE_CHIP = registerItem("system_override_chip", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(NullPointerEntity.MOD_ID, "system_override_chip")))));
+
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(NullPointerEntity.MOD_ID, name), item);
+    }
+
+    public static void registerModdedItems() {
+        NullPointerEntity.LOGGER.info("Building AURORA's components... " + NullPointerEntity.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(AURORA_CORE);
+            entries.add(CORRUPTED_DATA_SHARD);
+            entries.add(MEMORY_FRAGMENT);
+            entries.add(SYSTEM_OVERRIDE_CHIP);
+        });
+    }
+}
