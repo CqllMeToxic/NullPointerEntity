e5c28fef56a29b22cc34f153d1f0567b379ecaef
CqllMeToxic <idonuntknow@gmail.com>
2025-08-12 04:24:30 -0400

    Initial commit

---
+++ src/main/java/lol/cqllmetoxic/nullpointerentity/NullPointerEntity.java
@@ -0,0 +1,15 @@
+package lol.cqllmetoxic.nullpointerentity;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NullPointerEntity implements ModInitializer {
+	public static final String MOD_ID = "nullpointerentity";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/lol/cqllmetoxic/nullpointerentity/NullPointerEntityClient.java
@@ -0,0 +1,10 @@
+package lol.cqllmetoxic.nullpointerentity;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NullPointerEntityClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
