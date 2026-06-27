811e47af9029e67eba37a29cbdb335016a42cd80
CqllMeToxic <contact@cqllmetoxi.cc>
2026-06-26 21:47:23 -0400

    whoops forgot this one

---
+++ src/main/java/lol/cqllmetoxic/nullpointerentity/network/MultiplayerPolicy.java
@@ -0,0 +1,10 @@
+package lol.cqllmetoxic.nullpointerentity.network;
+
+/**
+ * multiplayer policy for a connected player session.
+ */
+public enum MultiplayerPolicy {
+    SAFE_ONLY,
+    IMMERSIVE_LOCAL_ALLOWED
+}
+
